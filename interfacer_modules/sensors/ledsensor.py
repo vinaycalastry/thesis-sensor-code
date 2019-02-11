9f31c19b9bc75942e465f1587151b45d38c9785f
Vinay <vinay.calastry@gmail.com>
2019-02-10 22:53:36 -0800

    Adding test code for blinking LED

--- interfacer_modules/sensors/ledsensor.py
+++ interfacer_modules/sensors/ledsensor.py
@@ -3,7 +3,6 @@
 import sys
 import RPi.GPIO as GPIO
 import time
-import threading
 
 class LEDSensor:
 
@@ -17,9 +16,6 @@ class LEDSensor:
         GPIO.setup(self.RED, GPIO.OUT)
         GPIO.setup(self.GREEN, GPIO.OUT)
         GPIO.setup(self.BLUE, GPIO.OUT)
-        self.e = threading.Event()
-        self.output = "100"
-        self.t = threading.Thread(name='non-block', target=self.flashLed)
 
     ## function to set initial LED state
     def set_led_init_state(self):
@@ -39,39 +35,17 @@ class LEDSensor:
         """
         This will make the LED blink red
         """
-        # GPIO.output(self.RED, 1)
-        # GPIO.output(self.GREEN, 0)
-        # GPIO.output(self.BLUE, 0)
-        self.output = "100"
-        self.t.start()
-        time.sleep(self.times_to_blink)
-        self.e.set()
+        GPIO.output(self.RED, 1)
+        GPIO.output(self.GREEN, 0)
+        GPIO.output(self.BLUE, 0)
 
 
     def blink_humidity_change(self):
         """
         This will make the LED blink blue
         """
-        # GPIO.output(self.RED, 0)
-        # GPIO.output(self.GREEN, 0)
-        # GPIO.output(self.BLUE, 1)
-        self.output = "001"
-        self.t.start()
-        time.sleep(self.times_to_blink)
-        self.e.set()
-
-    def flashLed(self):
-        new_list = list(self.output)
-        if new_list[0] == 1:
-            GPIO.output(self.RED, 1)
-            GPIO.output(self.GREEN, 0)
-            GPIO.output(self.BLUE, 0)
-        if new_list[2] == 1:
-            GPIO.output(self.RED, 0)
-            GPIO.output(self.GREEN, 0)
-            GPIO.output(self.BLUE, 1)
-        while not self.e.isSet():
-            time.sleep(0.5)
-            self.e.wait(self.t)
+        GPIO.output(self.RED, 0)
+        GPIO.output(self.GREEN, 0)
+        GPIO.output(self.BLUE, 1)
             
 
