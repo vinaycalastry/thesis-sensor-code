fc11c6c75e8b54f00a912be8349951b61a99b67a
Vinay <vinay.calastry@gmail.com>
2019-02-15 17:51:43 -0800

    Testing static values of temp/humidty

--- test_scripts/lcd_test.py
+++ test_scripts/lcd_test.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
-import I2C_LCD_driver
-from time import *
+import os
+import datetime
+import time
+
+## To import from interfacer_modules which is one directory above
+import sys
+sys.path.append("..")
+
+from interfacer_modules.sensors import I2C_LCD_driver
 
 mylcd = I2C_LCD_driver.lcd()
 
-mylcd.lcd_display_string("Hello World!", 1)
+mylcd.lcd_display_string("Temp: 56F", 1,1)
+mylcd.lcd_display_string("Humidity: 80%", 2,1)
