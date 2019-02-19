33491f1a136b8d47b58b5d7b7ef431ca3acc7ba1
Vinay <vinay.calastry@gmail.com>
2019-02-18 21:03:58 -0800

    trying out mqtt host as iotproducer

--- test_scripts/test_mqtt_lcd.py
+++ test_scripts/test_mqtt_lcd.py
@@ -48,7 +48,7 @@ def on_message(mosq, obj, msg):
     temp, humidity, time_recorded = json.loads(res)
     temp_in_f = celsius_to_fahrenheit(temp)
     lcd_sensor_instance.lcd_clear()
-    lcd_sensor_instance.lcd_display_string(str("Temp: "+str(temp_in_f)"F"), project_settings.TEMP_DISPLAY, project_settings.OFFSET)
+    lcd_sensor_instance.lcd_display_string(str("Temp: "+str(temp_in_f)+"F"), project_settings.TEMP_DISPLAY, project_settings.OFFSET)
     lcd_sensor_instance.lcd_display_string(str("Humidity: "+str(humidity)+"%"), project_settings.HUMIDITY_DISPLAY, project_settings.OFFSET)
     print("msg received: "+str(msg.payload))
     
