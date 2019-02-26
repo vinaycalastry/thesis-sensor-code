cdb455401c4841364fb5564ce2b97c9199dab252
Vinay <vinay.calastry@gmail.com>
2019-02-25 17:14:53 -0800

    added swarm.py to test

---
+++ test_scripts/test_swarm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+payload = {'temp': '23C', 'humidity': 85}
+x = json.dumps(payload)
+r = requests.post("http://localhost:8500/bzz:/",data=x , headers={'Content-Type': 'text/plain'})
+
+filehash = r.text
+#print(filehash)
+
+res = requests.get("http://localhost:8500/bzz:/"+filehash+"/")
+
+print(json.loads(res.text))
