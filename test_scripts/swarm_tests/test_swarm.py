e03c052285513b286f9e6b6ea566e8251564a3a7
Vinay <vinay.calastry@gmail.com>
2019-04-05 17:37:09 -0700

    Add all

--- test_scripts/swarm_tests/test_swarm.py
+++ test_scripts/swarm_tests/test_swarm.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 import requests
+requests.packages.urllib3.disable_warnings()
 import json
 
 payload = {'temp': '23C', 'humidity': 85}
 x = json.dumps(payload)
-r = requests.post("http://localhost:8500/bzz:/",data=x , headers={'Content-Type': 'text/plain'})
+r = requests.post("https://192.168.0.16/",data=x , headers={'Content-Type': 'text/plain'},verify=False)
 
 filehash = r.text
 
-res = requests.get("http://localhost:8500/bzz:/"+filehash+"/")
+res = requests.get("https://192.168.0.16/"+filehash+"/", verify=False)
 
 print(json.loads(res.text))
